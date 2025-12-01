58ddbd66bbf45a048ed9b76996838d4b35968c9f
Luan Souza <luan.souza@globalsys.com.br>
2025-12-01 02:02:35 -0300

    update httpClient service

--- src/service/axiosClient.ts
+++ src/service/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { MEDUSA_API_KEY, API_URL } from '@env';
 
-import { HttpClient } from './httpClient';
+import { IHttpClient } from './httpClient';
 
 const instace = axios.create({
   baseURL: API_URL,
@@ -11,11 +11,20 @@ const instace = axios.create({
   }
 })
 
-export const axiosClient: HttpClient = {
+export const axiosClient: IHttpClient = {
   async get(url, options) {
     try {
       const payload = await instace.get(url, options);
       return payload.data;
+    } catch (error) {
+      console.log('error', error);
+    }
+  },
+
+  async post(url, body) {
+    try {
+      const response = await instace.post(url, body);
+      return response.data;
     } catch (error) {
       console.log('error', error);
     }
--- src/service/httpClient.ts
+++ src/service/httpClient.ts
@@ -1,3 +1,7 @@
-export interface HttpClient {
-  get<T>(url: string, options?: any): Promise<T>;
+interface IHttpRequestOptions {
+  params?: Record<string, string>;
+}
+export interface IHttpClient {
+  get<T>(url: string, options?: IHttpRequestOptions): Promise<T>;
+  post<T, B>(url: string, body: B, options?: IHttpRequestOptions): Promise<T>;
 }
